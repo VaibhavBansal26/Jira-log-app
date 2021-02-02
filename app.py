d42069a72b6ba23ed8dd34c3463dc2128151874d
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2021-02-02 23:24:12 +0530

    final-modification

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/indexi')
-def indexi():
+@app.route('/')
+def index():
     return render_template('index.html')
+
+@app.route('/errors')
+def errors():
+    return render_template('error.html')
 
 @app.route('/login')
 def login():
@@ -67,8 +71,9 @@ def postWork():
         return jsonify({'trace': traceback.format_exc()})
     return render_template('postWork.html')
 
-@app.route('/',methods=['GET','POST'])
-def index():
+
+@app.route('/postWorklog',methods=['GET','POST'])
+def postWorklog():
     try:
         if request.method == 'POST':
             username = request.form['username']
@@ -78,8 +83,10 @@ def index():
             comment = request.form.getlist('comment[]')
             timeSpent = request.form.getlist('timeSpent[]')
             date = request.form.getlist('date[]')
-
-            #message = "VaibhavB2:Druv@family#21"
+            key1=key[1:]
+            comment1=comment[1:]
+            timeSpent1=timeSpent[1:]
+            date1=date[1:]
             message = username+":"+password
             message_bytes = message.encode('ascii')
             base64_bytes = base64.b64encode(message_bytes)
@@ -91,9 +98,38 @@ def index():
             print(date)
             m=datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
             print(m)
-            #r = datetime.datetime(2018, 9, 15)
-            #print(r.strftime("%b %d %Y %H:%M:%S"))
-            for a,b,c,d in zip(key,comment,timeSpent,date):
+            tSpent = []
+            for j in timeSpent:
+                j="".join(j.split())
+                l1=[]
+                m1=[]
+                s1=""
+                for i in j:
+                    if i.isalpha():
+                        m1.append(i)
+                        k1=int(s1)
+                        l1.append(k1)
+                        s1=""
+                    elif i.isdigit():
+                        s1+=i    
+                t1=0
+                for x1,y1 in zip(m1,l1):
+                    if x1=='w':
+                        q1=5*8*60*60*y1
+                        t1+=q1
+                    elif x1=='d':
+                        v1=8*60*60*y1
+                        t1+=v1
+                    elif x1=='h':
+                        u1=60*60*y1
+                        t1+=u1
+                    elif x1=='m':
+                        f1=y1*60
+                        t1+=f1
+                    elif x1=='s':
+                        t1+=y1
+                tSpent.append(t1)
+            for a,b,c,d in zip(key,comment,tSpent,date):
                 url = "https://jira.verifone.com/rest/api/2/issue/"+a+"/worklog"
                 headers = {
                 "Authorization": "Basic "+base64_message,
@@ -104,10 +140,13 @@ def index():
                 print(b)
                 print(c)
                 print(d)
+                date_time_obj = datetime.strptime(d,'%Y:%m:%d %H:%M:%S.%f').isoformat()
+                print(date_time_obj[:-3])
+                date_time_obj = date_time_obj[:-3]
                 payload = json.dumps( {
                 "timeSpentSeconds": c,
                 "comment":b,
-                "started":d+":00.065+0000"
+                "started":date_time_obj+"+0000"
                 } )
                 response = requests.request(
                 "POST",
@@ -115,13 +154,42 @@ def index():
                 data=payload,
                 headers=headers
                 )
-                print(payload)
-                print(response.status_code)
-            #nt=response.status_code
+                en1=response.status_code
+                print(en1)    
     except:
-        return jsonify({'trace': traceback.format_exc()})
+        return redirect(url_for('errors'))
 
-    return render_template('index.html')
+    return render_template('index.html',ent=en1,keys=key,comments=comment,timeSpents=timeSpent,dates=date,fol=zip(key1,comment1,timeSpent1,date1))
+
+
+'''
+@app.route('/postWorklog',methods=['GET','POST'])
+def postWorklog():
+    try:
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            key = request.form.getlist('key[]')
+            comment = request.form.getlist('comment[]')
+            timeSpent = request.form.getlist('timeSpent[]')
+            date = request.form.getlist('date[]')
+            message = username+":"+password
+            message_bytes = message.encode('ascii')
+            base64_bytes = base64.b64encode(message_bytes)
+            base64_message = base64_bytes.decode('ascii')
+            key1=key[1:]
+            comment1=comment[1:]
+            timeSpent1=timeSpent[1:]
+            date1=date[1:]
+            en1=201
+            print(en1)
+    except:
+        return redirect(url_for('errors'))jsonify({'trace': traceback.format_exc()})
+
+    return render_template('index.html',ent=en1,keys=key,comments=comment,timeSpents=timeSpent,dates=date,fol=zip(key1,comment1,timeSpent1,date1))
+
+'''
 
 
 @app.route('/getWorklog',methods=['GET','POST'])
@@ -163,7 +231,7 @@ def getWorklog():
     except:
         return jsonify({'trace': traceback.format_exc()})
 
-    return render_template('index.html',nt=t,key=key)
+    return render_template('indexi.html',nt=t,key=key)
 
 
 
