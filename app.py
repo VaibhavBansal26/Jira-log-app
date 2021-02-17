80c1a53d980c533573862bd1a456c6b190ed68d9
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2021-02-17 12:19:43 +0530

    final-time-debug5

--- app.py
+++ app.py
@@ -156,7 +156,8 @@ def postWorklog():
                 now=datetime.now()
                 local = tzlocal()
                 now = now.replace(tzinfo = local)
-                now_time = now.astimezone(local).strftime("%b-%d-%Y %H:%M:%S")
+                #now_time = now.astimezone(local).strftime("%b-%d-%Y %H:%M:%S")
+                now_time = now.astimezone(local).isoformat()
                 print(now_time)
             print(rc)
             w=[]
